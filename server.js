cee6c1238caf9706a5302bbc5e775b4c2c981f6d
Devin <leadpee@gmail.com>
2019-03-31 19:53:25 -0500

    meme neko

--- server.js
+++ server.js
@@ -142,35 +142,10 @@ function dankMeme(message) {
 }
 
 async function nekoCommand(message) {
-	var index = message.content.indexOf(" ");
-	if (index != -1) {
-		var nekoFunc = message.content.substr(index + 1);
-		console.log(nekoFunc);
-		if(nekoType === "neko") {
-			var pic = await neko.nsfw.neko();
-			message.channel.send(pic.url);
-		} else if (nekoType === "hentai") {
-			var pic = await neko.nsfw.hentai();
-			message.channel.send(pic.url);
-		} else if (nekoType === "trap") {
-			var pic = await neko.nsfw.trap();
-			message.channel.send(pic.url);
-		} else if (nekoType === "cumsluts") {
-			var pic = await neko.nsfw.cumsluts();
-			message.channel.send(pic.url);
-		} else if (nekoType === "smug") {
-			var pic = await neko.sfw.smug();
-			message.channel.send(pic.url);
-		} else if (nekoType === "gif") {
-			var pic = await neko.sfw.nekoGif();
-			message.channel.send(pic.url);
-		} else if (nekoType == "kiss") {
-			var pic = await neko.sfw.kiss();
-			message.channel.send(pic.url);
-		} else {
-			var pic = await neko.nsfw.neko();
-			message.channel.send(pic.url);
-		}
+	var rn = getRandomInt(1);
+	if (index != 1) {
+		var nekopic = await neko.nsfw.neko();
+		message.channel.send(nekopic.url);
 	} else {
 		var nekopic = await neko.sfw.neko();
 		message.channel.send(nekopic.url);
